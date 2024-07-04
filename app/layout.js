37aab0066f34c95ec972469548235658b53cc8e2
James Han <contactjameshan@gmail.com>
2024-07-04 16:20:03 -0500

    favicon update

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "James Han's Portfolio",
+  title: "James Han Portfolio",
   description: "James Han's personal portfolio website.",
 };
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/",
-  output: "export", // for dist folders
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
