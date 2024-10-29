d29141bcb17ec17f825f0d63b4a8163aa39f0ff8
James Han <contactjameshan@gmail.com>
2024-10-29 10:10:39 -0500

    timeline update

--- app/components/timeline.js
+++ app/components/timeline.js
@@ -1,69 +1,123 @@
 // components/WorkTimeline.js
 import Image from "next/image";
 
+// Create a FormattedText component
+const FormattedText = ({ type, children }) => {
+  switch (type) {
+    case "bold-underline":
+      return <span className="font-bold underline">{children}</span>;
+    case "bold":
+      return <span className="font-bold">{children}</span>;
+    case "underline":
+      return <span className="underline">{children}</span>;
+    default:
+      return <span>{children}</span>;
+  }
+};
+
 const timelineData = [
   {
     year: "2024 - Current",
     company: "Learning...",
     logo: "/photo/logo-popcat.jpeg",
-    description:
-      "Learning about Deep Learning and NLP through Andrew Ng's Deep Learning course and Andrej Kapathy's course.",
+    description: (
+      <>
+        Learning about Deep Learning and NLP through Andrew Ng's Deep Learning
+        course and{" "}
+        <a
+          href="https://www.youtube.com/@AndrejKarpathy"
+          className="text-blue-600 hover:underline"
+        >
+          Andrej Karpathy
+        </a>
+        's course.
+      </>
+    ),
   },
   {
     year: "2024 Sep - Current",
     company: "Univeristy of Texas at Dallas",
     logo: "/photo/logo-dallas.jpeg",
-    description:
-      "Just been part of Graduate Research Assistant for Dr. Latifur Khan, working on LLM Fairness Research.",
-  },
-  {
-    year: "2024 Aug - Current",
-    company: "Univeristy of Texas at Dallas",
-    logo: "/photo/logo-dallas.jpeg",
-    description:
-      "Studying Masters in Computer Science, focusing on Intelligent Systems (AI, ML, NLP).",
+    description: (
+      <>
+        M.Sc. at Unversity of Texas at Dallas, majoring Computer Science,
+        focusing on Intelligent Systems (AI, ML, NLP) and part of{" "}
+        <FormattedText type="bold">Graduate Research Assistant</FormattedText>{" "}
+        for Dr. Latifur Khan, working on LLM Fairness Research.
+      </>
+    ),
   },
   {
     year: "2024 June - 2024 Sep",
     company: "Amazon",
     logo: "/photo/logo-amazon.jpeg",
-    description:
-      "Went back for Software Development Intern at Amazon, worked with Kindle Team.",
+    description: (
+      <>Software Development Intern at Amazon, worked with Kindle Team.</>
+    ),
   },
   {
     year: "2020 - 2024",
     company: "Univeristy of Texas at Dallas",
     logo: "/photo/logo-dallas.jpeg",
-    description:
-      "Graduated B.Sc. for Computer Science, where I Researched under Prof. Latifur Khan as an Undergraduate, working on Imbalanced Data Classification with LLM.",
+    description: (
+      <>
+        B.Sc. at Unversity of Texas at Dallas majoring Computer Science, where I
+        was an{" "}
+        <FormattedText type="bold">Undergraduate Researcher</FormattedText>{" "}
+        under Prof. Latifur Khan as an , working on Imbalanced Data
+        Classification with LLM.
+      </>
+    ),
   },
   {
     year: "2023 Aug - 2024 May",
     company: "Samsung Research America",
     logo: "/photo/logo-samsung.jpeg",
-    description:
-      "Came back to Samsung as SWE Intern to continue to work on NEON, an Aritficial Human Assistant. My work with the team got featured in Huge News Stations. Also helped prototyping new non-released projects. ",
+    description: (
+      <>
+        Software Engineering Intern at Samsung and work on{" "}
+        <FormattedText type="bold">NEON</FormattedText>, an Aritficial Human
+        Assistant. My work with the team got featured in Huge News Stations.
+        Also helped prototyping new non-released projects.
+      </>
+    ),
   },
   {
     year: "2023 May - Aug",
     company: "Amazon",
     logo: "/photo/logo-amazon.jpeg",
-    description:
-      "Went to Amazon for Software Development Internship and worked with Recruiting Engine Team.",
+    description: (
+      <>
+        Software Development Intern at Amazon and worked with{" "}
+        <FormattedText type="bold">Recruiting Engine Team</FormattedText>.
+      </>
+    ),
   },
   {
     year: "2022 May - 2023 May",
     company: "Samsung Research America",
     logo: "/photo/logo-samsung.jpeg",
-    description:
-      "Software Engineer Intenrnshp at Team NEON, working on Aritficial Human Assistant. Prototyped and created packages and full stack applications for NEON system. ",
+    description: (
+      <>
+        Software Engineering Intern at Samsung, working with Team NEON,{" "}
+        <FormattedText type="bold">Artificial Human Assistant</FormattedText>.
+        Prototyped and created packages and full stack applications for NEON
+        system.
+      </>
+    ),
   },
   {
     year: "2022 Jan - 2022 May",
     company: "ACM UT Dallas",
     logo: "/photo/logo-acmutd.jpeg",
-    description:
-      "Worked as Undergraduate Researcher, researching Reinforcement Learning for Traffic Light Control under Dr. Yongwan Chun.",
+    description: (
+      <>
+        Worked as{" "}
+        <FormattedText type="bold">Undergraduate Researcher</FormattedText> at
+        ACM Research in UT Dallas, researching Reinforcement Learning for
+        Traffic Light Control under Dr. Yongwan Chun.
+      </>
+    ),
   },
 ];
 
@@ -91,7 +145,7 @@ export default function WorkTimeline() {
               <div className="w-px h-full bg-gray-200 -mt-2"></div>
             </div>
             <div className="flex-grow">
-              <h3 className="text-xl font-semibold mb-1">{item.company}</h3>
+              {/* <h3 className="text-xl font-semibold mb-1">{item.company}</h3> */}
               <p className="text-sm text-gray-500 mb-2">{item.year}</p>
               <p className="text-gray-700 leading-relaxed">
                 {item.description}
